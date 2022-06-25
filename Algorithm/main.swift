7cbf4ae6215887cad0f4ad2597c33383e07ed6b6
finleykim <g_kimmm@naver.com>
2022-06-25 16:29:46 +0900

    백준2579(다이나믹-계단오르기)-미제

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -6,28 +6,3 @@
 //
 
 import Foundation
-
-
-let testCase = Int(readLine()!)!
-var line = [[Int]]()
-
-
-for _ in 0..<testCase{
-    line.append(readLine()!.split(separator: " ").map{Int(String($0))!})
-}
-
-for i in 1..<testCase{
-    for j in 0...i{
-        if j == 0{
-            line[i][j] += line[i-1][j]
-        } else if j == i{
-            line[i][j] += line[i-1][j-1]
-        } else {
-            line[i][j] += max(line[i-1][j-1],line[i-1][j])
-        }
-    }
-}
-print(line[testCase-1].max()!)
-
-
-
