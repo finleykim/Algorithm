da960774f751631295ac7a7eab8b372416b337d3
finleykim <g_kimmm@naver.com>
2022-07-13 22:19:31 +0900

    Programmers: 레벨1 - 로또의 최고 순위와 최저순위

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -7,16 +7,34 @@
 
 import Foundation
 
-let id = ["muzi", "frodo", "apeach", "neo"]
-let re = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
-
-func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
-    var reported = [String : String]()
+func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
     
-for i in Set(report){
-    var splited = i.split(separator: " ").map{String($0)}
-    reported[splited[0]] = splited[1]
+    let minwoo = lottos.sorted()
+    let winNums = win_nums.sorted()
+    var sameNum = 0
+    
+    for i in minwoo{
+        for j in winNums{
+            if i < j || i > j{
+                continue
+            } else if i == j{
+                sameNum += 1
+            }
+        }
+    }
+    
+    switch sameNum{
+    case 6 : sameNum = 1
+    case 5 : sameNum = 2
+    case 4 : sameNum = 3
+    case 3 : sameNum = 4
+    case 2 : sameNum = 5
+    default : sameNum = 6
+    }
+    
+    return [0, sameNum]
 }
 
-    if reporte
-}
+print(solution([44,1,0,0,31,25], [31,10,45,1,6,19]))
+
+//MARK: 최대등수 맞추는 코드 넣으면 됨. 최소등수는 해쑴
