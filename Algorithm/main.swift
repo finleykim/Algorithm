9a628b803d9973338b8cbcc22c8762f9594ff561
finleykim <g_kimmm@naver.com>
2022-07-14 18:30:32 +0900

    프로그래머스(탐색-로또의 최고 순위와 최저순위)

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -7,34 +7,4 @@
 
 import Foundation
 
-func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
-    
-    let minwoo = lottos.sorted()
-    let winNums = win_nums.sorted()
-    var sameNum = 0
-    
-    for i in minwoo{
-        for j in winNums{
-            if i < j || i > j{
-                continue
-            } else if i == j{
-                sameNum += 1
-            }
-        }
-    }
-    
-    switch sameNum{
-    case 6 : sameNum = 1
-    case 5 : sameNum = 2
-    case 4 : sameNum = 3
-    case 3 : sameNum = 4
-    case 2 : sameNum = 5
-    default : sameNum = 6
-    }
-    
-    return [0, sameNum]
-}
 
-print(solution([44,1,0,0,31,25], [31,10,45,1,6,19]))
-
-//MARK: 최대등수 맞추는 코드 넣으면 됨. 최소등수는 해쑴
